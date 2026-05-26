6968f623afd37461a7b3082f55eb0a7ab42225d7
Àlvar Amat <alvar@additioapp.com>
2026-05-26 17:08:40 +0200

    chore: upgrade storybook + update pacakges manager

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,56 +1,64 @@
 const path = require('path')
+
 module.exports = {
   stories: [
-    '../stories/**/*.stories.mdx',
+    '../stories/**/*.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
+    '@storybook/addon-webpack5-compiler-babel',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    //'@storybook/addon-jest', // TODO: try this out
-    {
-      name: `@storybook/preset-scss`,
-      options: {
-        rule: {
-          test: /(?<!\.module).s[ca]ss$/,
-        },
-      },
-    },
-    // module
-    {
-      name: `@storybook/preset-scss`,
-      options: {
-        rule: {
-          test: /\.module\.s[ca]ss$/,
-        },
-        cssLoaderOptions: {
-          modules: {
-            localIdentName: '[name]__[local]--[hash:base64:5]',
-          },
-        },
-      },
-    },
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: 'webpack5',
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
   },
   webpackFinal: async (config) => {
     config.devtool = 'inline-source-map'
+
+    // MDX loader (addon-docs preset doesn't merge automatically in some setups)
+    config.module.rules.push({
+      test: /\.mdx$/,
+      exclude: /(stories|story)\.mdx$/,
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: { babelrc: true },
+        },
+        {
+          loader: require.resolve('@storybook/addon-docs/mdx-loader'),
+        },
+      ],
+    })
+
+    // Non-module SCSS
+    config.module.rules.push({
+      test: /(?<!\.module)\.s[ca]ss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    })
+
+    // Module SCSS
+    config.module.rules.push({
+      test: /\.module\.s[ca]ss$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              localIdentName: '[name]__[local]--[hash:base64:5]',
+            },
+          },
+        },
+        'sass-loader',
+      ],
+    })
 
     config.entry.unshift(
       path.resolve(__dirname, '../stories/resources/main.scss')
     )
 
-    // Aliases the src to the package name, so that 'example' scripting reads right
     config.resolve.alias['react-big-calendar'] = path.resolve(
       __dirname,
       '../src'
